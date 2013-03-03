2833f654b78afd694df7d13fcbd1a60380f2448e
rcrosbourne <rcrosbourne@gmail.com>
2013-03-03 11:34:56 -0500

    Data Access Layer provisional design

---
+++ GymSoftMembershipManager/DataAccessLayerLibrary/Action.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayerLibrary
+{
+    public class Action : Entity
+    {
+        public static override IEnumerable<Entity> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static override Entity Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static override int DeleteAll(int user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static override int Delete(int id, int user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static override int Insert(Entity entity, int user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static override int Update(Entity entity, int user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static override int BulkInsert(IEnumerable<Entity> entities, int user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static override bool IsNewRecord(Entity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GymSoftMembershipManager/DataAccessLayerLibrary/Entity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayerLibrary
+{
+    public abstract class Entity 
+    {
+        #region Public Properties
+        public int Id { get; private set; }
+        public DateTime InsertedAt { get; private set; }
+        public int InsertedBy { get; set; }
+        public DateTime UpdatedAt { get; private set; }
+        public int UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; } //The actual record isnt deleted only marked as deleted 
+        //TODO
+        //Error Handling procedures
+        #endregion
+
+        #region Static (Class methods)
+        public abstract static IEnumerable<Entity> FindAll();
+        public abstract static Entity Find(int id);
+        public abstract static int DeleteAll(int user); // Returns the number of rows marked as deleted
+        public abstract static int Delete(int id, int user); // Returns the number of rows marked as deleted
+        public abstract static int Insert(Entity entity, int user); //Returns the number of rows inserted 1 on success 0 on failure
+        public abstract static int Update(Entity entity, int user); //Returns the number of rows inserted 1 on success 0 on failure
+        public abstract static int BulkInsert(IEnumerable<Entity> entities, int user); // Returns the number of rows Inserted
+        public abstract static bool IsNewRecord(Entity entity); // Check if entity is new record or existing
+        #endregion 
+
+        #region Object Methods
+        public bool Save(int user)
+        {
+            bool result;
+            // Save the object to the database. Returns true if save was sucessful false otherwise
+            if (IsNewRecord(this))
+            {
+                result = Insert(this, user);
+            }
+            else
+            {
+                result = Update(this, user);
+            }
+            return result == 1;
+        }
+        public bool Delete(int user)
+        {
+            bool result = Delete(this.Id, user);
+            return result == 1;
+        }
+        #endregion
+
+
+        
+    }
+}
