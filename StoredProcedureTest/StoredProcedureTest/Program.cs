c4c78d3a25c373be275824eb844331b0ce93dc6f
Draco <dwayne.williams@gmail.com>
2013-03-14 16:48:59 -0500

    MYSQL stored procedures demos
    Added two projects to demonstrate how to connect to mysql database and
    execute stored procedures. Test1 accessed a stored procedure with in and
    out parameters. Test 2 access a stored procedure which returns a
    recordset and has a out parameter.

---
+++ StoredProcedureTest/StoredProcedureTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MySqlConnection oconn = new MySqlConnection("Server=gymsoft.db.10266153.hostedresource.com; Port=3306; Database=gymsoft; Uid=gymsoft; Pwd='G3tf!t12';");
+            
+            MySqlCommand command = oconn.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "gym_sp_AuthenticateUser";
+            command.Parameters.AddWithValue("buid", "1");
+            command.Parameters.AddWithValue("uname", "vulputate,");
+            command.Parameters.AddWithValue("pass", "Mauris");
+            command.Parameters.Add(new MySqlParameter("@uid", MySqlDbType.Int32));
+            command.Parameters["@uid"].Direction = ParameterDirection.Output;
+            //command.Parameters.Add(new MySqlParameter("@result", MySqlDbType.VarChar, 12));
+            command.Parameters.Add(new MySqlParameter("@result", MySqlDbType.Int32));
+            command.Parameters["@result"].Direction = ParameterDirection.Output;
+            command.Connection.Open();
+            command.ExecuteNonQuery();
+            command.Connection.Close();
+
+            System.Console.WriteLine(command.Parameters["@uid"].Value.ToString() + " " + command.Parameters["@result"].Value.ToString());
+
+
+            System.Threading.Thread.Sleep(5000);
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
