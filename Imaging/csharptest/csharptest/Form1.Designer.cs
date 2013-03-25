ffb59068ee65d3a4f9231fe6e62abcf41ae43159
Draco <dwayne.williams@gmail.com>
2013-03-25 15:14:52 -0500

    Added Imaging Sample code and DLL class project

---
+++ Imaging/csharptest/csharptest/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace csharptest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDevices = new System.Windows.Forms.ListBox();
+            this.picCapture = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.sfdImage = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstDevices
+            // 
+            this.lstDevices.FormattingEnabled = true;
+            this.lstDevices.ItemHeight = 16;
+            this.lstDevices.Location = new System.Drawing.Point(12, 73);
+            this.lstDevices.Name = "lstDevices";
+            this.lstDevices.Size = new System.Drawing.Size(287, 292);
+            this.lstDevices.TabIndex = 0;
+            // 
+            // picCapture
+            // 
+            this.picCapture.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picCapture.Location = new System.Drawing.Point(352, 73);
+            this.picCapture.Name = "picCapture";
+            this.picCapture.Size = new System.Drawing.Size(486, 343);
+            this.picCapture.TabIndex = 1;
+            this.picCapture.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 484);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(136, 35);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Preview";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(163, 484);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(136, 35);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop Preview";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(702, 484);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(136, 35);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 569);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.picCapture);
+            this.Controls.Add(this.lstDevices);
+            this.Name = "Form1";
+            this.Text = "Video Capture Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDevices;
+        private System.Windows.Forms.PictureBox picCapture;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.SaveFileDialog sfdImage;
+    }
+}
+
