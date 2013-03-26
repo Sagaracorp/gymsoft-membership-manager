23bad6dfc8ca2b42713cbbd4aa3d77bddfa36609
Rainaldo Crosbourne <rcrosbourne@gmail.com>
2013-03-26 16:08:11 -0500

    Storing users to database

--- GymSoft.CinchMVVM/GymSoft.UserModule/ViewModels/AddNewUserViewViewModel.cs
+++ GymSoft.CinchMVVM/GymSoft.UserModule/ViewModels/AddNewUserViewViewModel.cs
@@ -18,6 +18,75 @@ namespace GymSoft.UserModule.ViewModels
         private readonly IMessageBoxService messageBoxService;
         private readonly IUserService _userService;
 
+        private AsyncType asyncState = AsyncType.Content;
+        private bool isBusy;
+        private string waitText;
+        private string errorMessage;
+        /// <summary>
+        /// IsBusy
+        /// </summary>
+        static PropertyChangedEventArgs isBusyArgs =
+            ObservableHelper.CreateArgs<AddNewUserViewViewModel>(x => x.IsBusy);
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            private set
+            {
+                isBusy = value;
+                NotifyPropertyChanged(isBusyArgs);
+            }
+        }
+        /// <summary>
+        /// AsyncState
+        /// </summary>
+        static PropertyChangedEventArgs asyncStateArgs =
+            ObservableHelper.CreateArgs<AddNewUserViewViewModel>(x => x.AsyncState);
+
+        public AsyncType AsyncState
+        {
+            get { return asyncState; }
+            private set
+            {
+                asyncState = value;
+                NotifyPropertyChanged(asyncStateArgs);
+            }
+        }
+        /// <summary>
+        /// WaitText
+        /// </summary>
+        static PropertyChangedEventArgs waitTextArgs =
+            ObservableHelper.CreateArgs<AddNewUserViewViewModel>(x => x.WaitText);
+
+
+        public string WaitText
+        {
+            get { return waitText; }
+            private set
+            {
+                waitText = value;
+                NotifyPropertyChanged(waitTextArgs);
+            }
+        }
+
+
+        /// <summary>
+        /// ErrorMessage
+        /// </summary>
+        static PropertyChangedEventArgs errorMessageArgs =
+            ObservableHelper.CreateArgs<AddNewUserViewViewModel>(x => x.ErrorMessage);
+
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            private set
+            {
+                errorMessage = value;
+                NotifyPropertyChanged(errorMessageArgs);
+            }
+        }
+
         #region Commands
         public SimpleCommand<Object, Object> AddNewUserCommand { get; private set; }
         public SimpleCommand<Object, Object> CancelAddNewUserCommand { get; private set; }
@@ -65,15 +134,24 @@ namespace GymSoft.UserModule.ViewModels
         }
         private void ExecuteAddNewUserCommand(Object args)
         {
+            //AsyncState = AsyncType.Busy;
+            IsBusy = true;
+            WaitText = string.Format("Adding new user");
             _userService.CreateNewUserTask(this.NewUser,NewUserAdded, ErrorAddingNewUser);
             
         }
         private void NewUserAdded(int userId)
         {
+            IsBusy = false;
+            //AsyncState = AsyncType.Content;
             messageBoxService.ShowInformation(String.Format("New User Added with userId = {0}", userId));
+            NewUser = new User();
         }
         private void ErrorAddingNewUser(Exception exception)
         {
+            ErrorMessage = exception.Message;
+            AsyncState = AsyncType.Error;
+            IsBusy = false;
             messageBoxService.ShowError(exception.Message);
         }
         //void _viewAwareStatus_ViewLoaded()
